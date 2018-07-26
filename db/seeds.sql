ca1c6521c94b66cc0486f81ed3a9dae46c72fd54
Kristofernash <39805142+Kristofernash@users.noreply.github.com>
2018-07-25 23:11:49 -0700

    set up files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('5 guys');
+INSERT INTO burgers (burger_name) VALUES ('In-n-out');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jumbo Jack', true);
+INSERT INTO burgers (burger_name) VALUES ('mondo burger');
